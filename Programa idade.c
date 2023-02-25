ef35b3cd67fd5ab5d1e1382376404148a15cf59b
Jonatah Paz <125221259+JonatahPaz@users.noreply.github.com>
2023-02-25 20:42:07 -0300

    Add files via upload

---
+++ Programa idade.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+	int main(){
+
+int idade = 0;
+float peso = 0.0;
+
+
+	printf("Digite uma idade:\n");
+	scanf("%d.", &idade);
+	
+	printf("Digite um peso:\n");
+	scanf("%f.", &peso);
+	
+	printf("idade informada: %d.\n", idade);
+	printf("Peso informado: %f.\n", peso);
+	
+	system("pause");
+
+}
